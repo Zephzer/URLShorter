b84d8749f6fe79b76112d509c630cceca6331fbc
Michael <lilymichael87@gmail.com>
2023-06-21 19:33:53 +0800

    feat: add show page

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const URL = require('./models/urls')
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
@@ -15,13 +17,35 @@ db.once('open', () => {
     console.log('mongodb connected!')
 })
 
-app.engine('hbs', exphbs({ defaultLayout: 'main' , extname: '.hbs' }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.render('index')
-});
+    URL.find()
+       .lean()
+       .then(() => res.render('index'))
+       .catch(error => console.log(error))
+})
+
+app.post('/shorten', (req, res) => {
+    const longURL = req.body.url
+    // 生成隨機的短代碼
+    const shortCode = Math.random().toString(36).substr(2, 6)
+    // 構建縮短網址
+    const shortURL = "http://localhost:3000/" + shortCode
+    return URL.create({ shortURL, longURL })
+        .then(() => res.redirect('/show'))
+        .catch(error => console.log(error))
+})
+
+app.get('/show', (req, res) => {
+    URL.find({ longURL: '321' })
+       .lean()
+       .then(url => res.render('show', { url: url[0] }))
+       .catch(error =>  console.log(error))
+})
 
 app.listen(3000, () => {
     console.log(`App is running on http://localhost:3000`)
